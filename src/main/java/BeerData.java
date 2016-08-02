22bcb74b01d1066de1d93f824677cdf54e9a8db8
widzew <klop99@wp.pl>
2016-08-02 09:07:30 +0200

    Beerdomongo 1.0 arraylist

--- src/main/java/BeerData.java
+++ src/main/java/BeerData.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by widzew on 2016-07-30.
  */
@@ -7,18 +9,26 @@ public class BeerData {
     private double Percent;
     private double Plato;
 
-    public BeerData (String beerName, double Percent, double Plato) {
+    public BeerData(String beerName, double Percent, double Plato) {
         this.BeerName = beerName;
         this.Percent = Percent;
         this.Plato = Plato;
-}
+    }
+
 
     public String toString() {
-        return "Beer " + BeerName + " Percent " + Percent + " Plato "+ Plato;
+        return "Beer " + BeerName + " Percent " + Percent + " Plato " + Plato;
     }
 
-    public boolean isStrong() {
-        if (Percent > 4.9) return true;
-        else return false;
+
+
+    public BeerData(ArrayList<BeerData> beerList) {
+        if (Percent > 4.9) {
+            System.out.println("You can drink one more");
+        } else {
+            System.out.println("This beer is strong");
+        }
     }
+
+
 }
--- src/main/java/MainBeer.java
+++ src/main/java/MainBeer.java
@@ -1,34 +1,23 @@
+import java.util.ArrayList;
+
 /**
  * Created by widzew on 2016-07-30.
  */
 public class MainBeer {
     public static void main(String[] args) {
 
-        BeerData beer = new BeerData("Rebel Podwójnie Chmielone", 5, 12);
-        BeerData beer1 = new BeerData("Ciechan", 6.0, 12.2);
-        BeerData beer2 = new BeerData("Tenczynek Lager", 4.9, 12);
+        ArrayList<BeerData> beerList = new ArrayList<>();
+        beerList.add(new BeerData("Rebel Podwójnie Chmielone", 5, 12 ));
+        beerList.add(new BeerData("Ciechan", 6, 12.2 ));
+        beerList.add(new BeerData("Tenczynek Lager", 4.9, 12 ));
+
+        BeerData beer = new BeerData(beerList);
 
 
-        System.out.println(beer);
-        System.out.println(beer1);
-        System.out.println(beer2);
+        System.out.println(beerList);
 
-        if(beer.isStrong()) {
-            System.out.println("This beer is strong");
-        } else {
-            System.out.println("You can drink one more");
-        }
 
-        if(beer1.isStrong()) {
-            System.out.println("This beer is strong");
-        } else {
-            System.out.println("You can drink one more");
-        }
-        if(beer2.isStrong()) {
-            System.out.println("This beer is strong");
-        } else {
-            System.out.println("You can drink one more");
         }
 
     }
-}
+
